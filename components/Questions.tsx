0dc5c21dc28a20426a6c794caf0065d1aa2393c5
kippsss <keithchewzk@gmail.com>
2023-07-01 00:27:36 +0800

    use index for key in Questions

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -6,7 +6,6 @@ import { motion } from "framer-motion";
 export default function Questions() {
   const questions = [
     {
-      id: 0,
       question: "What is the adoption process like?",
       answer: [
         "1. Contact us through our socials to indicate your interest.",
@@ -19,21 +18,18 @@ export default function Questions() {
       ],
     },
     {
-      id: 1,
       question: "I'm interested to adopt, what do I do now?",
       answer: [
         "Great to hear! Please reach out to us through our socials below and we will get back to you as soon as we can.",
       ],
     },
     {
-      id: 2,
       question: "I'd like to volunteer, how may I help?",
       answer: [
         "Please get in touch with us through our socials below, we'd love to have you aboard!",
       ],
     },
     {
-      id: 3,
       question: "I sent a message but did not receive a reply, what's going on?",
       answer: [
         "We may have unintentionally missed your message (sorry!). Please feel free to give us a bump, or try another one of our socials.",
@@ -53,8 +49,8 @@ export default function Questions() {
           <h2 className="text-4xl font-bold text-center">FAQs</h2>
         </div>
         <Collapse.Group>
-          {questions.map((question) => (
-            <Collapse title={question.question} className="text-xl font-medium" key={question.id}>
+          {questions.map((question, index) => (
+            <Collapse key={index} title={question.question} className="text-xl font-medium">
               {question.answer.map((line) => (
                 <Text className="font-normal">{line}</Text>
               ))}
